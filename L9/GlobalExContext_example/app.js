d94300095c9aea884daf8b718e4e33f7b9062ebc
zekeApps <sheqel@live.com>
2015-08-12 12:07:40 -0500

    Lectures renamed as L

---
+++ L9/GlobalExContext_example/app.js
@@ -0,0 +1,10 @@
+// Global Execution Context example
+// every code lexical sitted outside any function becomes part of the Global Execution Context
+
+var a = "Hello World"
+
+function b(){
+
+}
+
+// a equal to window.a equal to "Hello World"
